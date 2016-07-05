f36b585ddbd420e66cec3978bb76185b53771b72
natarajgandhi <natarajgandhi@gmail.com>
2016-07-04 22:21:32 -0400

    Initial Commit

---
+++ static/js/sjta.js
@@ -0,0 +1,10 @@
+﻿$(window).scroll(function () {
+    var s = $(window).scrollTop(),
+          d = $(document).height(),
+          c = $(window).height();
+    scrollPercent = (s / (d - c)) * 100;
+    var position = scrollPercent;
+
+    $("#progress").width(position + '%');
+
+});
